f83ece392e1dc029708aa8c6972cd58d82e3b878
dayana <pipicanodayana2005@gmail.com>
2024-05-07 14:17:43 -0500

    crud de cargo

--- apps/reservas/views.py
+++ apps/reservas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from apps.reservas.models import Rol
+from apps.reservas.models import Rol,TipoDocumento,Cargo
 from django.core.paginator import Paginator
 def Home(request):
     return render(request,'home.html')
@@ -63,3 +63,97 @@ def listar_rol(request):
 #FUNCIONES DE TIPO DE DOCUMENTO 
 
 #FUNCION DE CREAR TIPO DE DOCUMENTO
+
+def create_tipo_documento(request):
+    
+    nombre = TipoDocumento.POST.get('nombre')
+    if request.method == 'POST':
+        tipo_documento = TipoDocumento.objects.create(
+            nombre = nombre
+        )
+        
+        return redirect('listar_tipo_documento')
+    
+    return render(request,'TipoDocumento/create_tipo_documento.html')
+
+#FUNCION DE ACTUALIZAR TIPO DOCUMENTO
+        
+def update_tipo_documento(request,id):
+    
+    tipo_documento = TipoDocumento.objects.get('nombre')
+    
+    if request.method == 'POST':
+        nombre = nombre
+        
+        tipo_documento.nombre = nombre
+        tipo_documento.save()
+    
+        return redirect('listar_tipo_documento')
+    return render(request, 'TipoDocumento/update_tipo_documento.html', {'tipo_documento':tipo_documento})
+
+#FUNCION DE ELIMINAR TIPO DE DOCUMENTO
+def delete_tipo_documento(request,id):
+    tipo_documento = TipoDocumento.objects.get(id=id)
+    if request.method == 'GET':
+        tipo_documento.delete()
+        
+        return redirect('listar_tipo_documento')
+
+#FUNCION DE LISTAR TIPO DE DOCUMENTO
+def listar_tipo_documento(request):
+    tipo_documento = TipoDocumento.objects.all()
+    
+    paginate = Paginator(tipo_documento,10)
+    
+    page_number = request.GET.get('page')
+    page_obj =  paginate.get_page(page_number)
+    
+    return render(request, 'TipoDocumento/listar_tipo_documento', {'page_obj':page_obj}, {'tipo_documento':tipo_documento})
+
+#FUNCIONES DE CARGO 
+
+#FUNCION DE CREAR CARGO
+
+def create_cargo(request):
+    
+    nombre = Cargo.POST.get('nombre')
+    
+    if request.method == 'POST':
+        
+        cargo = request.objects.create(
+            nombre = nombre
+        )
+        return redirect('listar_cargo')
+    
+    return render(request, 'Cargo/create_cargo.html')
+
+def update_cargo(request,id):
+    cargo = Cargo.objects.get(id=id)
+    
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        
+        cargo.nombre = nombre
+        
+        return redirect('listar_cargo')
+    return render(request,'Cargo/update_cargo.html', {'cargo':cargo})
+
+def delete_cargo(request,id):
+    
+    cargo = Cargo.objects.get(id=id) 
+    
+    if request.method == 'GET':
+        cargo.delete
+        
+        return redirect('listar_cargo')
+
+def listar_cargo(request,id):
+    
+    cargo = Cargo.objects.all()
+    
+    paginacion = Paginator(cargo,10)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginacion.get_page(page_number)
+    
+    return render(request,'Cargo/listar_cargo', {'page_obj':page_obj}, {'cargo':cargo})
--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.reservas.views import Home ,Registro,create_rol,update_rol,delete_rol,listar_rol
-
+from apps.reservas.views import Home ,Registro
+from apps.reservas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     
@@ -27,12 +27,28 @@ urlpatterns = [
     #path('login/', login, name='login' ),
     #path('logaut/', logaut, name='logaut'),
     
+    
+    
     #RUTAS DE ROL
     
-    path('rol/create/', create_rol, name='create_rol'),
-    path('rol/update/<int:id>', update_rol, name='update_rol'),
-    path('rol/delete/<int:id>', delete_rol, name='delete_rol'),
-    path('rol/listar/', listar_rol, name='listar_rol'),
+    path('rol/create/', views.create_rol, name='create_rol'),
+    path('rol/listar/', views.listar_rol, name='listar_rol'),
+    path('rol/update/<int:id>', views.update_rol, name='update_rol'),
+    path('rol/delete/<int:id>', views.delete_rol, name='delete_rol'),
+    
+    #RUTAS DE TIPO DOCUMENTO
+    
+    path('TipoDocumento/create/', views.create_tipo_documento, name='create_tipo_documento'),
+    path('TipoDocumento/update/<int:id>', views.update_tipo_documento, name='update_tipo_documento'),
+    path('TipoDocumento/delete/<int:id>', views.delete_tipo_documento, name='delete_tipo_documento'),
+    path('TipoDocumento/listar/', views.listar_tipo_documento, name='listar_tipo_documento'),
+    
+    #RUTAS DE CARGO
+    
+    path('cargo/create', views.create_cargo, name='create_cargo'),
+    path('cargo/update', views.update_cargo, name='update_cargo'),
+    path('cargo/delete', views.delete_cargo, name='delete_cargo'),
+    path('cargo/listar', views.listar_cargo, name='listar_cargo'),
     
 
 ]
