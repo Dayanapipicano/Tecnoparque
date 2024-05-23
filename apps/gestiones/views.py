4233b5165510ce6a68839d1a9ce2d5ebaa271c02
Jhoan C <stivelco@gmail.com>
2024-05-23 09:58:21 -0500

    Se crearon formulario de Registro y Recuperar contraseña. Creacion de style.css para las paginas mencionadas

--- apps/gestiones/views.py
+++ apps/gestiones/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request,'home.html')
-   
-   
-    
+
+def registro(request):
+    return render(request, 'registro.htrml')
--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     # path('gestion/', include('apps.gestiones.urls')),
     # path('funcionario/', include('apps.funcionarios.urls')),
     # path('reserva/', include('apps.reservas.urls')),
